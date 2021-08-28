a2b0c14d90ef3675972c2567e272087e25e5064e
Storm-Eagle20 <johnfausnaught@gmail.com>
2021-08-28 19:23:34 -0400

    Update nineball.js

--- nineball.js
+++ nineball.js
@@ -6,7 +6,7 @@ const botChannel = "570160968643117077" //"517934719170641930"; // #staff-bot-ch
 
 const storm = "160648230781059073"; // @Storm Eagle
 
-const patterns = require("./regex.js"); // grab the regex filters
+const {small, full} = require("./regex.js"); // grab the regex filters
 
 // checks if the message isn't from the bot itself and not
 // from a private message
@@ -17,15 +17,53 @@ bot.on("messageCreate", async msg => {
 	// checks public messages against regex filters
 	if (msg.channel.id == logsChannel || msg.channel.id == botChannel) return
 	
-	let result = patterns.findIndex(pattern => pattern.test(msg.cleanContent));
+	let result = full.findIndex(pattern => pattern.test(msg.cleanContent));
+	var pattern = full[result];
 	
+	var backupFilter = false;
+	
+	if (result == -1) {
+		let count = 0
+		for (let pattern of small) {
+			for (let match of msg.content.matchAll(pattern)) {
+				count += 1
+			}
+		}
+	};
+	if (count > 2) {
+		backupFilter = true;
+	};
+	if (backupFilter == true) {
+		if (msg.author.id == storm) { // don't attempt to ban storm for testing this lol
+			console.log("This test passed.");
+		}
+		else (msg.member.ban(0, "scammer").catch(console.error)) // ban the scammer 
+			
+		guild.channels.get(logsChannel).createMessage({ // create a message regarding the details
+			embed: {
+				"description": `Banned a scammer! Loose match.`,
+				"color": 1715584,
+				"footer": {
+					"icon_url": "https://vignette.wikia.nocookie.net/armoredcore/images/0/02/Hustler_One_Emblem.jpg/revision/latest?cb=20140615012341",
+					"text": "Target verified. Commencing hostilities!"
+					},
+				"fields": [ 
+					{
+					"name": `**${msg.author.username}#${msg.author.discriminator}:**`, // discord ID
+					"value": `${msg.cleanContent}` // copy of the scam message
+					}
+				]
+			}
+		});
+		msg.delete().catch(console.error); // delete offending message. it's put after the log message to avoid any potential errors.
+	}
 	if (result > -1) {
 		const guild = msg.channel.guild;
 		// checks if the ID is from The Raven
 		if (msg.author.id == storm) {	
 			guild.channels.get(logsChannel).createMessage({
 				embed: {
-					"description": `Testing successful. Matched result: \n${result}`, // for testing filters
+					"description": `Testing successful. Matched result: \n\`\`\`\n${pattern.source}\n\`\`\``, // for testing filters
 					"color": 1715584,
 					"footer": {
 						"icon_url": "https://vignette.wikia.nocookie.net/armoredcore/images/0/02/Hustler_One_Emblem.jpg/revision/latest?cb=20140615012341",
@@ -41,11 +79,11 @@ bot.on("messageCreate", async msg => {
 			});
 		}
 		else {
-			msg.member.ban(0, "scammer") // ban the scammer 
+			msg.member.ban(0, "scammer").catch(console.error) // ban the scammer 
 			
 			guild.channels.get(logsChannel).createMessage({ // create a message regarding the details
 				embed: {
-					"description": `Banned a scammer! Matched result: \n${result}`,
+					"description": `Banned a scammer! Matched result: \n\n${pattern.source}`,
 					"color": 1715584,
 					"footer": {
 						"icon_url": "https://vignette.wikia.nocookie.net/armoredcore/images/0/02/Hustler_One_Emblem.jpg/revision/latest?cb=20140615012341",
@@ -59,7 +97,7 @@ bot.on("messageCreate", async msg => {
 					]
 				}
 			});
-			msg.delete(); // delete offending message. it's put after the log message to avoid any potential errors.
+			msg.delete().catch(console.error); // delete offending message. it's put after the log message to avoid any potential errors.
 	    }
 	};
 });
