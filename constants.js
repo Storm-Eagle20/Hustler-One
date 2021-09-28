8af80de24ab5c3d921cc8a4cb69d2152714afb22
Storm-Eagle20 <johnfausnaught@gmail.com>
2021-09-28 14:46:26 -0400

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,13 @@
+const storm = "160648230781059073"; // @Storm Eagle
+
+const logChannels = {
+	"195214576571121664": "201369951775227904", //mega man server ID
+	"508647842592587776": "508655085648216075", //fanworks server ID
+	"176362550311518208": "363004532311064578", //ps homebrew server ID
+	"297536872589295626": "387117292053463041"  //xbox homebrew server ID
+};
+
+module.exports = {
+	storm,
+	logChannels
+}
