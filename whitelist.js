2aeeea322c429cb9af34bfcc32a753e74185fc59
Storm-Eagle20 <johnfausnaught@gmail.com>
2022-01-22 21:12:24 -0500

    Add files via upload
    whitelist file; used by nine ball to avoid scanning anything with ubiquitous topics

---
+++ whitelist.js
@@ -0,0 +1,19 @@
+whitelist = [
+    /mega/gi,
+    /rock/gi,
+    /roll/gi,
+    /steampowered\.com/gi,
+    /ps/gi,
+    /homebrew/gi,
+    /xbox/gi,
+    /dev/gi,
+    /nintendo/gi,
+    /sony/gi,
+    /microsoft/gi,
+    /exploit/gi,
+    /tool/gi,
+]
+
+module.exports = {
+    whitelist
+}
