d52bef81e948766316a65532414f626b9f752114
Storm-Eagle20 <johnfausnaught@gmail.com>
2022-01-22 21:08:31 -0500

    Update commands.js
    add some new commands as detailed in the file

--- commands.js
+++ commands.js
@@ -1,14 +1,15 @@
 const {small, full} = require("./regex.js");
-const {saveRegex, messageScan, errorMessage, embedList, postMessage} = require("./functions.js");
+const {whitelist} = require("./whitelist.js");
+const {saveRegex, saveWhitelist, messageScan, errorMessage, embedList, postMessage, countBans, uploadFiles} = require("./functions.js");
 
-const update = /^9b(?: (commands|list|remove|modify))?(?: ([1-9][0-9]*|0))?(?: `(.+)`)?( \-generic)?/ //update command, explained below
+const update = /^9b(?: (commands|list|remove|modify|scamcount|upload))?(?: ([1-9][0-9]*|0))?(?: `(.+)`)?( \-generic| \-whitelist)?/ //update command, explained below
 
-//nbCommand[1] is (commands|list|remove|modify)
+//nbCommand[1] is (commands|list|remove|modify|scamcount|upload)
 //nbCommand[2] is ([1-9][0-9]*)
 //nbCommand[3] is (.+)
 //nbCommand[4] is ( \-generic)
 
-let regexType = "test"; 
+let regexMessage = "test"; 
 //this is a variable used 
 //in an embed when Nine Ball is updated.
 
@@ -20,6 +21,15 @@ function nbCommands(msg) {
 	
 	let nbCommand = msg.content.match(update)
 	if (!nbCommand) return;
+	
+	let embedColor = false //used later to determine red or green embed color
+	
+	if (nbCommand[4]) { //whether to update generic or whitelist filter
+		if (nbCommand[4] == " -generic") {
+			regexType = small
+		}
+		else {regexType = whitelist}
+	}
 	
 	if (nbCommand[1] == "commands") { //"9b commands"
 		guild.channels.get(channelId).createMessage({
@@ -48,12 +58,20 @@ function nbCommands(msg) {
 					"value": `Lists all generic filters.`
 					},
 					{
+					"name": `9b list -whitelist`,
+					"value": `Lists the whitelist.`
+					},
+					{
 					"name": `9b [regex here]`,
 					"value": `Adds a new targeted filter. REPLACE BRACKETS WITH BACKTICKS!`
 					},
 					{
 					"name": `9b [regex here] -generic`,
 					"value": `Adds a new generic filter. REPLACE BRACKETS WITH BACKTICKS!`
+					},
+					{
+					"name": `9b [regex here] -whitelist`,
+					"value": `Adds a new term to the whitelist. REPLACE BRACKETS WITH BACKTICKS!`
 					},
 					{
 					"name": `9b remove x`,
@@ -64,35 +82,51 @@ function nbCommands(msg) {
 					"value": `Removes a generic filter based on index number.`
 					},
 					{
+					"name": `9b remove x -whitelist`,
+					"value": `Removes a whitelisted term based on index number.`
+					},
+					{
 					"name": `9b modify x [regex here]`,
 					"value": `Replaces a targeted filter based on index number. REPLACE BRACKETS WITH BACKTICKS!`
 					},
 					{
 					"name": `9b modify x [regex here] -generic`,
 					"value": `Replaces a generic filter based on index number. REPLACE BRACKETS WITH BACKTICKS!`
+					},
+					{
+					"name": `9b modify x [regex here] -whitelist`,
+					"value": `Replaces a whitelist term based on index number. REPLACE BRACKETS WITH BACKTICKS!`
+					},
+					{
+					"name": `9b scamcount`,
+					"value": `Lists amount of scams since last bootup.`
+					},
+					{
+					"name": `9b upload`,
+					"value": `Uploads regex files.`
 					}
 				]}
 		})
 		return
 	}
 	else if (nbCommand[1] == "list") { //list the regex
-		if (nbCommand[4]) { //generic regex filter 
-			let indexLength = small.length;
-			let lines = small.map((regex, index) => `${index}: ${regex}`)
-			embedList(guild, channelId, indexLength, lines);
+		if (nbCommand[4]) { //generic or whitelist regex filter 
+			let indexLength = regexType.length;
+			let lines = regexType.map((regex, index) => `${index}: ${regex}`)
+			embedList(channelId, guild, indexLength, lines);
 			return
 		}
 		else {
 			let indexLength = full.length; //targeted regex filter
 			let lines = full.map((regex, index) => `${index}: ${regex}`)
-			embedList(guild, channelId, indexLength, lines);
+			embedList(channelId, guild, indexLength, lines);
 			return
 		}
 	}
 	else if (nbCommand[1] == "remove") { //remove a regex filter
 		indexNumber = nbCommand[2];
 		if (nbCommand[4]) { //generic regex splicing
-			let smallSize = parseInt(small.length)
+			let smallSize = parseInt(regexType.length)
 
 			if (indexNumber > smallSize) {
 				errorMessage(channelId, guild);
@@ -102,10 +136,21 @@ function nbCommands(msg) {
 				errorMessage(channelId, guild);
 				return //check for invalid values
 			}
-			small.splice(indexNumber, 1); //remove specified value
-			saveRegex();
-			regexType = "Generic filter removed."
-			postMessage(guild, channelId, regexType); //post confirmation message
+			
+			if (nbCommand[4] == " -generic") {
+				small.splice(indexNumber, 1); //remove specified value
+				saveRegex();
+				regexMessage = "Generic filter removed."
+			}
+			else {
+				whitelist.splice(indexNumber, 1); //remove specified value
+				saveWhitelist();
+				regexMessage = "Whitelist filter removed."
+			}
+			
+			embedColor = true
+			
+			postMessage(channelId, guild, regexMessage, embedColor); //post confirmation message
 			return
 		}
 		else { //targeted regex splicing
@@ -120,8 +165,9 @@ function nbCommands(msg) {
 			}
 			full.splice(indexNumber, 1); //remove specified value
 			saveRegex();
-			regexType = "Targeted filter removed."
-			postMessage(guild, channelId, regexType); //post confirmation message
+			regexMessage = "Targeted filter removed."
+			embedColor = true
+			postMessage(channelId, guild, regexMessage, embedColor); //post confirmation message
 			return
 		}
 	}
@@ -129,9 +175,8 @@ function nbCommands(msg) {
 		indexNumber = nbCommand[2];
 		if (nbCommand[4]) { //generic regex editing
 			try {
-				let regexChanges = new RegExp(nbCommand[3], "g")
-				let smallSize = parseInt(small.length)
-
+				let regexChanges = new RegExp(nbCommand[3], "gi")
+				let smallSize = parseInt(regexType.length)
 				if (indexNumber > smallSize) {
 					errorMessage(channelId, guild);
 					return
@@ -140,20 +185,29 @@ function nbCommands(msg) {
 					errorMessage(channelId, guild);
 					return //check for invalid values
 				}
-				small.splice(indexNumber, 1, regexChanges); //remove specified value
+				
+				if (nbCommand[4] == " -generic") {
+					small.splice(indexNumber, 1, regexChanges); //remove specified value
+					regexMessage = "Generic filter updated."
+					saveRegex(small);
+				}
+				else {
+					whitelist.splice(indexNumber, 1, regexChanges);
+					regexMessage = "Whitelist updated."
+					saveWhitelist(whitelist);
+				}
 			}
 			catch (error) {
+				console.log(error)
 				errorMessage(channelId, guild); //account for undefined/errors
 				return
 			}
-			saveRegex(small);
-			regexType = "Generic filter updated."
-			postMessage(guild, channelId, regexType); //post confirmation message
+			postMessage(channelId, guild, regexMessage, embedColor); //post confirmation message
 			return
 		}
 		else { //targeted regex editing
 			try {
-				let regexChanges = new RegExp(nbCommand[3], "g")
+				let regexChanges = new RegExp(nbCommand[3], "gi")
 				let smallSize = parseInt(full.length)
 
 				if (indexNumber > smallSize) {
@@ -171,32 +225,47 @@ function nbCommands(msg) {
 					return
 				}
 			saveRegex(full);
-			regexType = "Targeted filter updated."
-			postMessage(guild, channelId, regexType); //post confirmation message
+			regexMessage = "Targeted filter updated."
+			postMessage(channelId, guild, regexMessage, embedColor); //post confirmation message
 			return
 		}
+	}
+	else if (nbCommand[1] == "scamcount") {
+		countBans(channelId, guild)
+		return
+	}
+	else if (nbCommand[1] == "upload") {
+		uploadFiles(channelId, guild)
+		return
 	}
 	else if (nbCommand[3]) { //update the regex filter with new regular expressions
 		let newRegex = null;
 		try {
-			newRegex = new RegExp(nbCommand[3], "i")
+			newRegex = new RegExp(nbCommand[3], "gi")
 		}
 		catch (error) {
 			errorMessage(channelId, guild); //account for undefined/errors
 			return
 		}
 		if (nbCommand[4]) {
-			small.push(newRegex) //push to generic regex filter
-			regexType = "Update successful. New generic regular expression added.";
-			saveRegex(small); //save to file
+			if (nbCommand[4] == " -generic") {
+				small.push(newRegex) //push to generic regex filter
+				regexMessage = "Update successful. New generic regular expression added.";
+				saveRegex(small); //save to file
+			}
+			else {
+				whitelist.push(newRegex)
+				regexMessage = "Update successful. New whitelist added.";
+				saveWhitelist(whitelist);
+			}
 		}
 		else {
 			full.push(newRegex) //push to targeted regex filter
-			regexType = "Update successful. New targeted regular expression added.";
+			regexMessage = "Update successful. New targeted regular expression added.";
 			saveRegex(full); //save to file
 		}
 		
-		postMessage(guild, channelId, regexType); //post confirmation message
+		postMessage(channelId, guild, regexMessage, embedColor); //post confirmation message
 		
 		return
 	}
